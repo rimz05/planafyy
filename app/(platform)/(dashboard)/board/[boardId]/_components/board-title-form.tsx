a740ddac2f1edf909d48a8cbb7eb6af867229b99
rimz05 <139325866+rimz05@users.noreply.github.com>
2025-07-06 22:40:50 +0530

    git lit start

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -1,7 +1,14 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
+import {useRef, useState } from "react";
 import { Board } from "@prisma/client";
+
+import { Button } from "@/components/ui/button"
+import { FormInput } from "@/components/form/form-input";
+import { useAction } from "@/hooks/use-action";
+import { updateBoard } from "@/actions/update-board";
+import { toast } from "sonner";
+import { error } from "console";
 
 interface BoardTitleFormProps {
     data: Board;
@@ -10,11 +17,69 @@ interface BoardTitleFormProps {
 export const BoardTitleForm = ({
     data,
 }:  BoardTitleFormProps) =>{
+
+    const {execute} = useAction( updateBoard, {
+
+        onSuccess: (data) =>{
+            toast.success(`Board "${data.title}" updated!`)
+            setTitle(data.title)
+            disableEditing();
+        },
+
+        onError: (error) =>{
+            toast.error(error)
+        }
+    })
+
+    const formRef = useRef<HTMLFormElement>(null); 
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [title, setTitle] = useState(data.title)
+    const enableEditing = () =>{
+        setIsEditing(true);
+        setTimeout(() =>{
+            inputRef.current?.focus();
+            inputRef.current?.select();
+        })
+    }
+
+    const disableEditing = () =>{
+        setIsEditing(false)
+    }
+
+    const onSubmit = (FormData : FormData) =>{
+        const title = FormData.get("title") as string
+        execute({
+            title,
+            id: data.id,
+        })
+
+    }
+
+    const onBlur = () =>{
+        formRef.current?.requestSubmit();
+    }
+
+    if(isEditing){
+        return(
+            <form action={onSubmit} ref={formRef} className="flex items-center gap-x-2">
+                <FormInput
+                ref={inputRef}
+                id="title"
+                onBlur={onBlur}
+                defaultValue={title}
+                className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent border-none"
+                />
+            </form>
+        )
+    }
     return (
         <Button
+        onClick={enableEditing}
         variant="transparent"
         className="font-bold text-lg h-auto w-auto p-1 px-2">
-            {data.title}
+            {title} 
         </Button>
     )
 }
