712a5437e2681d76432d6762f14e435fd5da8343
rimz05 <139325866+rimz05@users.noreply.github.com>
2025-07-10 23:05:55 +0530

    list completed

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,11 +1,51 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { ListContainer } from "./_components/list-container";
 
+interface BoardIdPageProps{
+    params:{
+        boardId: string;
+    }
+}
 
+const BoardIdPage = async({
+    params,
+}: BoardIdPageProps) =>{
 
-const BoardIdPage = () =>{
+    const { orgId } = await auth();
+
+    if(!orgId){
+        redirect("/select-org")
+    }
+
+    const lists = await db.list.findMany({
+
+        where:{
+            boardId: params.boardId,
+            board:{
+                orgId,
+            }
+        },
+
+        include:{
+             cards:{
+                orderBy:{
+                    order:"asc"
+                }
+             }
+        },
+        orderBy:{
+            order:"asc"
+        }
+    })
 
     return(
-        <div className="h-full">
-            Board Id
+        <div className="p-4 overflow-x-auto h-screen lg:h-full ">
+            <ListContainer
+            boardId = {params.boardId}
+            data = {lists}
+            />
         </div>
     )
 }
---
+++ types.ts
@@ -0,0 +1,6 @@
+import { Card, List } from "@prisma/client";
+
+
+export type ListWithCards = List & {card: Card[]}
+
+export type CardWithList = Card & {list: List}
